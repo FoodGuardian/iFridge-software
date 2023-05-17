164472f5a4de215bf91458b312415de5a1a400cc
Kay-Westerhof <kay.westerhof@student.nhlstenden.com>
2023-05-17 16:09:45 +0200

    Scan barcode, temp shows scanned productcode

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import customtkinter as ctk
+import tkinter as tk
+from tkinter import messagebox
+from imutils.video import VideoStream
+from pyzbar import pyzbar
+import datetime
+import imutils
+import time
+import cv2
+import threading
 
 class Window(ctk.CTk):
     def __init__(self):
@@ -7,11 +16,12 @@ class Window(ctk.CTk):
         self.attributes("-fullscreen", True)
         self.bind("<Escape>", quit)
 
+
 def mainmenu():
     global main
     main = Window()
 
-    main.columnconfigure((0,1), weight=1, uniform="a")
+    main.columnconfigure((0, 1), weight=1, uniform="a")
     main.rowconfigure(0, weight=1, uniform="a")
     main.rowconfigure((1, 2), weight=2, uniform="a")
 
@@ -29,7 +39,9 @@ def mainmenu():
 
     main.mainloop()
 
+
 def productscan():
+    global result
     productscanwindow = Window()
 
     productscanwindow.columnconfigure((0, 3), weight=1, uniform="a")
@@ -37,14 +49,41 @@ def productscan():
     productscanwindow.rowconfigure((0), weight=1, uniform="a")
     productscanwindow.rowconfigure((1, 2), weight=2, uniform="a")
 
-    backbutton = ctk.CTkButton(productscanwindow, text="Terug", command= lambda: productscanwindow.destroy())
+    backbutton = ctk.CTkButton(productscanwindow, text="Terug", command=lambda: productscanwindow.destroy())
     backbutton.grid(row=0, column=0, sticky="nw", padx=5, pady=5)
 
     scantitle = ctk.CTkLabel(productscanwindow, text="Product scannen", font=("default", 32))
     scantitle.grid(row=0, column=1, columnspan=2, sticky="new", padx=20, pady=10)
 
+    button1 = ctk.CTkButton(productscanwindow, text="Scan", font=("default", 24), command=lambda: threading.Thread(target=scanproduct).start())
+    button1.grid(row=1, column=0, sticky="news", padx=20, pady=10, columnspan=2)
+
+    result = ctk.CTkLabel(productscanwindow, text="Result: ", font=("default", 24))
+    result.grid(row=1, column=2, sticky="new", padx=20, pady=10, columnspan=2)
 
     productscanwindow.mainloop()
+    #Lijkt er op dat de code na de mainloop niet uitgevoerd wordt
+
+
+def scanproduct():
+    global barcodeData
+    global result
+    vs = VideoStream(usePiCamera=True).start()
+    time.sleep(2.0)
+    scanning = True
+    while scanning:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        barcodes = pyzbar.decode(frame)
+        for barcode in barcodes:
+            barcodeData = barcode.data.decode("utf-8")
+            #barcodeType = barcode.type
+            print(barcodeData)
+            if(barcodeData != None):
+                scanning = False
+    #messagebox.showinfo("Productcode", barcodeData)
+    vs.stop()
+    result.configure(text=barcodeData)
 
 def productlist():
     productlistwindow = Window()
@@ -52,16 +91,16 @@ def productlist():
     productlistwindow.columnconfigure((0, 3), weight=1, uniform="a")
     productlistwindow.columnconfigure((1, 2), weight=2, uniform="a")
     productlistwindow.rowconfigure((0), weight=1, uniform="a")
-    productlistwindow.rowconfigure((1, 2), weight=2, uniform="a")
+    productlistwindow.rowconfigure((1, 2,), weight=2, uniform="a")
 
-    backbutton = ctk.CTkButton(productlistwindow, text="Terug", command= lambda: productlistwindow.destroy())
+    backbutton = ctk.CTkButton(productlistwindow, text="Terug", command=lambda: productlistwindow.destroy())
     backbutton.grid(row=0, column=0, sticky="nw", padx=5, pady=5)
 
     listtitle = ctk.CTkLabel(productlistwindow, text="Productenlijst", font=("default", 32))
     listtitle.grid(row=0, column=1, columnspan=2, sticky="new", padx=20, pady=10)
 
-
     productlistwindow.mainloop()
+
 
 def settings():
     global settingswindow
@@ -73,17 +112,16 @@ def settings():
     settingswindow.columnconfigure((0, 3), weight=1, uniform="a")
     settingswindow.columnconfigure((1, 2), weight=2, uniform="a")
     settingswindow.rowconfigure((0), weight=1, uniform="a")
-    settingswindow.rowconfigure((1, 2), weight=2, uniform="a")
+    settingswindow.rowconfigure((1, 2, 3, 4), weight=2, uniform="a")
 
-    backbutton = ctk.CTkButton(settingswindow, text="Terug", command= lambda: settingswindow.destroy())
+    backbutton = ctk.CTkButton(settingswindow, text="Terug", command=lambda: settingswindow.destroy())
     backbutton.grid(row=0, column=0, sticky="nw", padx=5, pady=5)
 
     settingstitle = ctk.CTkLabel(settingswindow, text="Instellingen", font=("default", 32))
     settingstitle.grid(row=0, column=1, columnspan=2, sticky="new", padx=20, pady=10)
 
-    quitbutton = ctk.CTkButton(settingswindow, text="Quit", command= lambda: quitall(), font=("defaut", 24))
+    quitbutton = ctk.CTkButton(settingswindow, text="Quit", command=lambda: quitall(), font=("defaut", 24))
     quitbutton.grid(row=1, column=1, columnspan=2, sticky="news", padx=20, pady=10)
-
 
     settingswindow.mainloop()
 
